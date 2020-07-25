3bec56f6cbee10f1bc12c23dd60b786edbc0ca42
mustafa tokmak <mtokmak06@gmail.com>
2020-07-26 01:17:03 +0300

    .

--- app/app/app.py
+++ app/app/app.py
@@ -1,13 +1,7 @@
 from model import db
-<<<<<<< HEAD
-from model import Client,Booking
+from model import Client,Booking,Flight,Airport,Route
 from model import app
 from flask import request
-=======
-from model import User
-from model import app
-
->>>>>>> 66176a3afc23ebd355f9f10bbceeca536b3b29bc
 import random
 import json
 
@@ -17,11 +11,10 @@ def hello_world():
 
 
 
-<<<<<<< HEAD
 
 
 
-@app.route('/register_as_client', methods=['GET','POST'])
+@app.route('/api/register_as_client', methods=['GET','POST'])
 def register_as_client():
     response = {}
     content = request.get_json()
@@ -45,7 +38,7 @@ def register_booking():
     content = request.get_json()
     for i in range(100):
         client = Client.query.all()[i%10]
-
+        
         print(client)
         booking = Booking(client_id=client.id)
         db.session.add(booking)
@@ -53,39 +46,41 @@ def register_booking():
     return json.dumps(response)
 
 
-if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
-=======
-@app.route('/app', methods=['GET'])
-@app.route('/app', methods=['GET'])
-def main():
+
+@app.route('/add_airports', methods=['GET','POST'])
+def add_airports():
     response = {}
-    for i in range(100):
-        username = "admin" + str(random.randint(0,10000000000))
-        email = "admin@example.com" + str(random.randint(0,10000000000))
-        admin = User(username=username, email=email)
-        db.session.add(admin)
+    content = request.get_json()
+    for i in range(1):
+        name = "esenboga" + str(random.randint(0,10000000000))
+        city = "ankara" + str(random.randint(0,10000000000))
+        country = "turkey"+str(random.randint(0,10000000000))
+        code = "ESB" + str(random.randint(0,10000000000))
+        airport = Airport(name=name,city=city,country=country,code=code)
+        print(airport)
+        db.session.add(airport)
         db.session.commit()
-    print(User.query.all())
-    for i in User.query.all():
-        print(i)
-    print(User.query.filter_by().first())
-    print(User.query.filter_by().first().email)
-    """try:
-        #TODO sürekli istek atılmasına karşşı önlem alınacak
-        remote_ip = request.remote_addr
-        response = {"ip":str(remote_ip)}
-        return json.dumps(response)
-    except Exception as e:
-        send_devops_mail(function="",email="",userid="",tenantid="",tenant_name="",error_type=str(request.url),platform="",content=str(request.get_data()))
-        logger.exception("public_ip")
-        response["ip"] = ""
-        response["result"] = "False"
-        response["message"] =  "public_ip error"""
     return json.dumps(response)
+
+
+
+@app.route('/get_reservations', methods=['GET','POST'])
+def get_reservations():
+    response = {}
+    content = request.get_json()
+    str_list =[]
+    for i in range(1):
+        client = Client.query.all()[i]
+        for r in client.reservations:
+            print(r)
+            str_list.append(str(r.id))
+    response["str_list"] = str_list
+    return json.dumps(response)
+
+
+
+
 
 if __name__ == "__main__":
     db.create_all()
-    app.run(debug=False, host='0.0.0.0', port=5000)
->>>>>>> 66176a3afc23ebd355f9f10bbceeca536b3b29bc
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- app/app/model.py
+++ app/app/model.py
@@ -11,10 +11,6 @@ db = SQLAlchemy(app)
 
 
 
-class Booking(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
-    client = db.relationship("Client", back_populates="reservations")
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     phone_number = db.Column(db.String(80))
@@ -27,14 +23,109 @@ class Client(db.Model):
         return '<client_id %r>' % self.id  
 
 
-        #flight_id = db.Column(db.Integer, db.ForeignKey('flight.flight_id'))
-    
-"""
-class Flight(db.Model):
-    flight_id = db.Model(db.Integer, primary_key=True)
-    #flight_team_id = 
-    reservations = db.relationship("Booking")
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
+    client = db.relationship("Client", back_populates="reservations")
 
+    flight_id = db.Column(db.Integer, db.ForeignKey('flight.id'))
+    flight = db.relationship("Flight", back_populates="bookings")
+    def __repr__(self):
+        return '<booking_id %r>' % self.id  
+    
+    
+    
+
+class Flight(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    bookings = db.relationship("Booking",back_populates="flight")
+
+    route_id = db.Column(db.Integer, db.ForeignKey('route.id'))
+    route = db.relationship("Route", back_populates="flights")
+
+    aircraft_id = db.Column(db.Integer, db.ForeignKey('aircraft.id'))
+    aircraft = db.relationship("Aircraft", back_populates="used_flights")
+
+    actual_departure_date_time = db.Column(db.Date)
+    actual_arrival_date_time = db.Column(db.Date)
+    
+    estimated_departure_date_time = db.Column(db.Date)
+    estimated_arrival_date_time = db.Column(db.Date)
+    
+
+class Aircraft(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number_of_seats = db.Column(db.Integer)
+    company = db.Column(db.String(120), unique=True, nullable=False)#table eklenecek
+    model = db.Column(db.String(120), unique=True, nullable=False)#table eklenecek
+    used_flights = db.relationship("Flight", back_populates="aircraft")
+    checks = db.relationship("Check", back_populates="aircraft")
+class Check(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date=db.Column(db.Date)
+    is_checked = db.Column(db.Boolean)
+    aircraft_id = db.Column(db.Integer, db.ForeignKey('aircraft.id'))
+    aircraft = db.relationship("Aircraft", back_populates="checks")
+
+    technician_id = db.Column(db.Integer, db.ForeignKey('technician.id'))
+    technician = db.relationship("Technician", back_populates="responsible_checks")
+    
+class Technician(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    responsible_checks = db.relationship("Check", back_populates="technician")
+    firstname = db.Column(db.String(120))
+    lastname = db.Column(db.String(120))
+    salary = db.Column(db.Integer)
+
+    
+
+
+
+
+
+class Departure_Airport(db.Model):
+    __tablename__ = 'departure_airport'
+    id = db.Column(db.Integer, primary_key=True)
+    route = db.relationship("Route", back_populates="departure_airport")
+
+    airport_id = db.Column(db.Integer, db.ForeignKey('airport.id'))
+    dep_airport = db.relationship("Airport", back_populates="as_departure")
+    
+class Arrival_Airport(db.Model):
+    __tablename__ = 'arrival_airport'
+    id = db.Column(db.Integer, primary_key=True)
+    route = db.relationship("Route", back_populates="arrival_airport")
+    
+    airport_id = db.Column(db.Integer, db.ForeignKey('airport.id'))
+    ar_airport = db.relationship("Airport", back_populates="as_arrival")
+    
+
+class Route(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    flights = db.relationship("Flight", back_populates="")
+    departure_airport_id = db.Column(db.Integer, db.ForeignKey('departure_airport.id'))
+    departure_airport = db.relationship("Departure_Airport", back_populates="route")
+    
+    arrival_airport_id = db.Column(db.Integer, db.ForeignKey('arrival_airport.id'))
+    arrival_airport = db.relationship("Arrival_Airport", back_populates="route")
+
+
+
+class Airport(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    as_arrival = db.relationship("Arrival_Airport",back_populates="ar_airport")
+    as_departure =  db.relationship("Departure_Airport",back_populates="dep_airport")
+    
+
+    name = db.Column(db.String(80), nullable=False)
+    city = db.Column(db.String(80), nullable=False) #table oluşturulacak
+    country = db.Column(db.String(80), nullable=False) #table oluşturulacak
+    code = db.Column(db.String(80), nullable=False)
+    def __repr__(self):
+        return '<Airport_name %r>' % self.name  
+
+
+"""
 class Fligt_Team(db.Model):
     pilot =  db.Model(db.Integer, primary_key=True)
     #estimated_departure_date = 
