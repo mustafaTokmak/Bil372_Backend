05553ec6b11669daa435cbbcd7086fe4dfed4562
mustafa tokmak <mtokmak06@gmail.com>
2020-07-25 17:21:30 +0300

    .

--- app/app/app.py
+++ app/app/app.py
@@ -1,7 +1,7 @@
 from model import db
-from model import User
+from model import Client,Booking
 from model import app
-
+from flask import request
 import random
 import json
 
@@ -12,33 +12,40 @@ def hello_world():
 
 
 
-@app.route('/app', methods=['GET'])
-@app.route('/app', methods=['GET'])
-def main():
+
+
+@app.route('/register_as_client', methods=['GET','POST'])
+def register_as_client():
     response = {}
-    for i in range(100):
-        username = "admin" + str(random.randint(0,10000000000))
+    content = request.get_json()
+    for i in range(10):
+        client_id = random.randint(0,100000000)
+        phone_number = "admin" + str(random.randint(0,10000000000))
         email = "admin@example.com" + str(random.randint(0,10000000000))
-        admin = User(username=username, email=email)
-        db.session.add(admin)
+        password = str(random.randint(0,10000000000))
+        first_name = "mustafa" + str(random.randint(0,10000000000))
+        
+        client = Client(phone_number=phone_number,email=email,password=password,first_name=first_name)
+        db.session.add(client)
         db.session.commit()
-    print(User.query.all())
-    for i in User.query.all():
-        print(i)
-    print(User.query.filter_by().first())
-    print(User.query.filter_by().first().email)
-    """try:
-        #TODO sürekli istek atılmasına karşşı önlem alınacak
-        remote_ip = request.remote_addr
-        response = {"ip":str(remote_ip)}
-        return json.dumps(response)
-    except Exception as e:
-        send_devops_mail(function="",email="",userid="",tenantid="",tenant_name="",error_type=str(request.url),platform="",content=str(request.get_data()))
-        logger.exception("public_ip")
-        response["ip"] = ""
-        response["result"] = "False"
-        response["message"] =  "public_ip error"""
     return json.dumps(response)
 
+
+
+@app.route('/register_booking', methods=['GET','POST'])
+def register_booking():
+    response = {}
+    content = request.get_json()
+    for i in range(100):
+        client = Client.query.all()[i%10]
+        
+        print(client)
+        booking = Booking(client_id=client.id,client=client)
+        db.session.add(booking)
+        db.session.commit()
+    return json.dumps(response)
+
+
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    db.create_all()
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- app/app/model.py
+++ app/app/model.py
@@ -5,15 +5,42 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/mustafa/test.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mustafa:magic_pass@postgres_container:5432/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mustafa:magic_pass@localhost:5432/test'
+app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+
+class Booking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
+    client = db.relationship("Client", back_populates="reservations")
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False) #HASH
+    first_name = db.Column(db.String(80))
+    reservations = db.relationship("Booking", back_populates="client")
 
     def __repr__(self):
-        return '<User %r>' % self.username  
+        return '<client_id %r>' % self.id  
 
+
+        #flight_id = db.Column(db.Integer, db.ForeignKey('flight.flight_id'))
+    
+"""
+class Flight(db.Model):
+    flight_id = db.Model(db.Integer, primary_key=True)
+    #flight_team_id = 
+    reservations = db.relationship("Booking")
+
+class Fligt_Team(db.Model):
+    pilot =  db.Model(db.Integer, primary_key=True)
+    #estimated_departure_date = 
+class Route(db.Model):
+    route_id = db.Model(db.Integer, primary_key=True)
+    #departure_airport_id = 
+    arrival_airport_id =
+class Airport(db.Model):
+    airport_id = db.Column(db.DateTime)"""
